3bedebfef9d5bb0ecbb86a37d089e8b700b2702c
Lukas Deutz <lukas.deutz@wirecard.com>
2019-12-18 14:21:50 +0100

    Fix missing log folder

--- stages/setup/main.sh
+++ stages/setup/main.sh
@@ -4,9 +4,10 @@ set -e
 source "${GITHUB_WORKSPACE}/toolchain/utils/bash_utils.sh"
 
 LOGDIR="/tmp/logs"
+mkdir -p "$LOGDIR"
 _main() {
     for SCRIPT in "${GITHUB_WORKSPACE}/toolchain/stages/setup/setup.d/"*.sh; do
-        local LOGFILE="${LOGDIR}/$(basename ${SCRIPT})"
+        local LOGFILE="${LOGDIR}/$(basename "${SCRIPT}" .sh).txt"
         source "${SCRIPT}"
 
         echo -n "Installing ${NAME}... "
