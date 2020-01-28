1552ff39c12dd3d8bfd1a13de1e4300eaf3b852d
Herbert Knapp <herbert.knapp@wirecard.com>
2020-01-28 14:35:01 +0100

    Set content_path var scope to all class instances

--- lib/utils.rb
+++ lib/utils.rb
@@ -1,16 +1,23 @@
 module Toolchain
+  @@content_path
+
   ##
-  # content_path
-  # Returns path to content directory +content_dir_path+.
+  # Sets content path across all instances
   #
-  def self.content_path(path = nil)
+  def initialize
     content_dir_path = '..'
     content_dir_path = ENV['GITHUB_WORKSPACE'] \
       if ENV.key?('TOOLCHAIN_TEST') || ENV.key?('GITHUB_ACTIONS')
     content_dir_path = ENV['CONTENT_PATH'] if ENV.key?('CONTENT_PATH')
-    # For Unit testing:
-    content_dir_path = path unless path.nil?
-    return content_dir_path
+    @@content_path = content_dir_path
+  end
+
+  ##
+  # Returns path to content directory +@@content_path+.
+  #
+  def self.content_path(path = nil)
+    @@content_path unless path.nil?
+    path
   end
 end
 
