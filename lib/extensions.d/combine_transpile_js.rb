e71941f336bf29eafabee699e496628a377c3882
Herbert Knapp <herbert.knapp@wirecard.com>
2020-01-28 14:36:13 +0100

    Update combine_transpile_js.rb and tests [skip ci]

--- lib/extensions.d/combine_transpile_js.rb
+++ lib/extensions.d/combine_transpile_js.rb
@@ -8,6 +8,8 @@ module Toolchain
   ##
   # Adds modules for preprocessing files.
   class CombineAndTranspileJs < BaseExtension
+    @docinfo_header_name_default = 'docinfo.html'
+    @docinfo_footer_name_default = 'docinfo-footer.html'
     ##
     # Combines js files referenced in docinfo{,-footer}.html to a single .js file
     # and transpiles them with BabelJS
@@ -16,35 +18,63 @@ module Toolchain
     def combine_and_transpile_js(docinfo_filepaths = nil)
       content_path = ::Toolchain.content_path
       docinfo_header_path = if docinfo_filepaths.nil?
-                              content_path + '/docinfo.html'
+                              content_path + '/' + @docinfo_header_name_default
                             else
                               docinfo_filepaths.header
                             end
 
       docinfo_footer_path = if docinfo_filepaths.nil?
-                              content_path + '/docinfo.html'
+                              content_path + '/' + @docinfo_footer_name_default
                             else
                               docinfo_filepaths.footer
                             end
-      js_files = Dir[content_path + '/js/*.js']
-      pp get_script_src_from_html_file(docinfo_header_path)
+      #js_header_files = Dir[content_path + '/js/header.js.d/*.js']
+      [docinfo_header_path, docinfo_footer_path].each do |docpath|
+        replace_js_files_with_blob(docpath)
+      end
     end
 
     ##
-    # parses html file and returns array +script_sources+ of all script tags' src attribute
-    # e.g. <script src="js/1.js"> --> ['js/1.js']
+    # Replaces all js tags in an html file +path+ with a tag that includes one big blob js.
+    # Writes to file and returns +path+ or nil if an error occured.
+    def replace_js_files_with_blob(path)
+      sources = get_script_src_from_html_file(path)
+      js_blob = sources.map do |s|
+        File.read(s)
+      end.join("\n\n")
+      puts js_blob
+      # check if head tag exits ===> isHeader
+    end
+
+    ##
+    # Parses html file +path+ loking for javascript files
+    #
+    # Returns +script_source_files+ array containing "src" attribute values of script 
+    #   e.g. <script src="js/1.js"> --> ['js/1.js']
     def get_script_src_from_html_file(path)
       unless File.file?(path)
         # raise Exception.new("Could not read html file " + path)
       end
       doc = File.open(path) { |f| Nokogiri::HTML(f) }
-      script_tags = doc.xpath('//script')
-      script_tags.each do |tag|
-        log('JS', 'skipping script tag without "src" attribute', :magenta) unless tag.key?('src')
-        # TODO: add check if script has src but children. then its a sloppy html code.
-        pp tag['src']
+      file = File.basename(path)
+      script_source_files = doc.xpath('//script').map do |s|
+        line_nr = s.line.to_s
+        unless s.key?('src')
+          log('JS', "#{file}:#{line_nr}" + ' skipping script tag without "src" attribute.', :yellow)
+          next
+        end
+        unless File.exist?(s.attribute('src'))
+          log('JS', "#{file}:#{line_nr}" + ' skipping tag, src not found: ' + s.attribute('src'), :yellow)
+          next
+        end
+        unless s.children.empty?
+          log('JS', "#{file}:#{line_nr}" + ' skipping invalid script tag.', :yellow)
+          next
+        end
+        s.attribute('src')
       end
-      script_tags
+      script_source_files = script_source_files.compact # remove nil
+      return script_source_files
     end
 
     def run(docinfo_filepaths = nil)
--- test/test_extensions.rb
+++ test/test_extensions.rb
@@ -289,37 +289,41 @@ end
 
 class TestJsCombineAndTranspile < Test::Unit::TestCase
   ##
-  # Create two samples js files each for a test docinfo.html and test docinfo-footer.html
-  # Then combine and transpile and check results
+  # Creates two samples js files each for a test docinfo.html and test docinfo-footer.html
+  # Adds invalid script tags
+  # Then combines and transpiles and check results
+  #
   def test_combine_and_transpile_js
     # TODO: outsource writing stuff to its own method
     js_header_files_contents = ['[1, 2, 3].map(n => n ** 2);', 'var [a,,b] = [1,2,3];']
     js_header_files_paths = []
     js_header_files_contents.each_with_index do |script, i|
-      js_header_files_paths << write_tempfile('js_' + i.to_s + '.js', script)
+      js_header_files_paths << write_tempfile('js_header_' + i.to_s + '.js', script)
     end
-    html_header = '<html><head>'
+    html_header = "<html><head>\n"
+    html_header += '<script src="invalid-file.js"></script>' + "\n"
     js_header_files_paths.each do |path|
-      html_header += '<script src="' + path + '"></script>'
+      html_header += '<script src="' + path + '"></script>' + "\n"
     end
-    html_header += '</head>'
-    html_header_filepath = write_tempfile('html_head.html', html_header)
+    html_header += '</head>' + "\n"
+    html_header_filepath = write_tempfile('docinfo_head.html', html_header)
 
     js_footer_files_contents = js_header_files_contents.reverse
     js_footer_files_paths = []
     js_footer_files_contents.each_with_index do |script, i|
-      js_footer_files_paths << write_tempfile('js_' + i.to_s + '.js', script)
+      js_footer_files_paths << write_tempfile('js_footer_' + i.to_s + '.js', script)
     end
     html_footer = ''
     js_footer_files_paths.each do |path|
-      html_footer += '<script src="' + path + '">'
+      html_footer += '<script src="' + path + '"></script>' + "\n"
     end
+    html_footer += '<script src="invalid-footer-file.js"></script>' + "\n"
     html_footer += '</body></html>'
-    html_footer_filepath = write_tempfile('html_footer.html', html_footer)
+    html_footer_filepath = write_tempfile('docinfo_footer.html', html_footer)
 
     docinfo_files_paths = OpenStruct.new('header' => html_header_filepath, 'footer' => html_footer_filepath)
     errors = Toolchain::CombineAndTranspileJs.new.run(docinfo_files_paths)
-    pp errors
+    #TODO: proper asserts, not just number of errors
     assert_equal(3, errors.length)
   end
 
