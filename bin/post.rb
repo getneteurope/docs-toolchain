7e3f41f8b3bb7bba0a7a5e2158b640063554d1d0
Lukas Deutz <lukas.deutz@wirecard.com>
2020-01-31 15:49:00 +0100

    Implement pre.rb and post.rb in bin/ and add Rakefile tasks

---
+++ bin/post.rb
@@ -0,0 +1,28 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+require_relative '../lib/cli.rb'
+require_relative '../lib/process_manager.rb'
+require_relative '../lib/log/log.rb'
+Dir[File.join(__dir__, '../', 'lib', 'post.d', '*.rb')].each { |file| require file }
+
+def main(argv = ARGV)
+  args, opt_parser = Toolchain::Process::CLI.parse_args(argv)
+  if args.help
+    puts opt_parser
+    exit 0
+  end
+
+  if args.list || args.debug
+    log('LOADED POST-PROCESSING', 'loaded processes:')
+    Toolchain::PostProcessManager.instance.get.each do |proc|
+      log('PROC', proc.class.name)
+    end
+    exit 0
+  end
+
+  stage_log(:post, 'Starting post-processing stage')
+  Toolchain::PostProcessManager.instance.run
+end
+
+main
