cf5470bc559f4670b2670bff5db2027581c25111
Herbert Knapp <herbert.knapp@wirecard.com>
2020-01-31 11:27:05 +0100

    Add missing content_path method to Toolchain module again

--- lib/base_extension.rb
+++ lib/base_extension.rb
@@ -3,6 +3,21 @@
 require_relative './extension_manager.rb'
 
 module Toolchain
+  require 'zlib'
+  ##
+  # content_path
+  # Returns path to content directory +content_dir_path+.
+  #
+  def self.content_path(path = nil)
+    content_dir_path = '..'
+    content_dir_path = ENV['GITHUB_WORKSPACE'] \
+      if ENV.key?('TOOLCHAIN_TEST') || ENV.key?('GITHUB_ACTIONS')
+    content_dir_path = ENV['CONTENT_PATH'] if ENV.key?('CONTENT_PATH')
+    # For Unit testing:
+    content_dir_path = path unless path.nil?
+    return content_dir_path
+  end
+
   # Location describes a source location, made up of +filename+ and +lineno+.
   # Params:
   # * +filename+: adoc filename
