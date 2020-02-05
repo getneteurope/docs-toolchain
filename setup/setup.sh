6bc7e474c3a40458e3dde66e0ac3d5c1a02a4a9c
Lukas Deutz <lukas.deutz@wirecard.com>
2020-02-05 17:07:45 +0100

    Cleanup bash code for setup

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,35 +1,38 @@
 #!/bin/bash
 
 set -e
-source "${TOOLCHAIN_PATH}/utils/bash_utils.sh"
+
+function log() {
+  echo $@
+}
 
 LOGDIR="/tmp/logs"
 mkdir -p "${LOGDIR}"
 export LOG_NOJSON=true #log_append cannot use log_append during setup phase
 
 _main() {
-    for SCRIPT in "${TOOLCHAIN_PATH}/setup/setup.d/"*.sh; do
-        local LOGFILE="${LOGDIR}/$(basename "${SCRIPT}" .sh).txt"
-        source "${SCRIPT}"
+  for SCRIPT in "${TOOLCHAIN_PATH}/setup/setup.d/"*.sh; do
+    local LOGFILE="${LOGDIR}/$(basename "${SCRIPT}" .sh).txt"
+    source "${SCRIPT}"
 
-        echo
-        if [[ -z $DISABLE ]]; then
-            log "Installing ${NAME}... "
-            _setup | tee "${LOGFILE}"
-        else
-            log "Skipping ${NAME}..."
-            log "Reason: disabled"
-        fi
+    echo
+    if [[ -z $DISABLE ]]; then
+      log "Installing ${NAME}... "
+      _setup | tee "${LOGFILE}"
+    else
+      log "Skipping ${NAME}..."
+      log "Reason: disabled"
+    fi
 
-        RETVAL=$?
-        if ((RETVAL != 0)); then
-            log ERROR "Failed!"
-            log "$(cat ${LOGFILE})"
-            return ${RETVAL}
-        else
-            log "OK"
-        fi
-    done
+    RETVAL=$?
+    if ((RETVAL != 0)); then
+      log ERROR "Failed!"
+      log "$(cat ${LOGFILE})"
+      return ${RETVAL}
+    else
+      log "OK"
+    fi
+  done
 }
 
 _main
