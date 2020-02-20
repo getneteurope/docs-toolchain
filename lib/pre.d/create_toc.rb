3535ce653792d0e1f4a1ada61e58cfed63918c9c
Lukas Deutz <lukas.deutz@wirecard.com>
2020-02-20 10:56:28 +0100

    Refactor create_toc.rb

--- lib/pre.d/create_toc.rb
+++ lib/pre.d/create_toc.rb
@@ -2,7 +2,12 @@
 
 require_relative '../process_manager.rb'
 require_relative '../base_process.rb'
+require_relative '../config_manager.rb'
+require_relative '../utils/adoc.rb'
+require_relative '../log/log.rb'
 require 'json'
+
+CM = Toolchain::ConfigManager.instance
 
 module Toolchain
   module Pre
@@ -10,33 +15,45 @@ module Toolchain
     # Adds modules for preprocessing files.
     class CreateTOC < BaseProcess
       ##
-      # Creates a TOC json file from an +adoc+ object
-      # Default json path is taken from config
+      # Creates a TOC JSON file from an +adoc+ object
+      # Default JSON path is taken from +ConfigManager+.
       #
       # Returns toc as json tree +toc_json+
-      def run(adoc, json_filepath = Toolchain::ConfigManager.instance.get('toc.file'))
+      def run(
+        adoc = nil, # Toolchain::Adoc.load_doc(CM.get('index.default.file')),
+        json_filepath = CM.get('toc.file')
+      )
+        # TODO document this bit since it's quite confusing
+        stage_log(:pre, '[Create TOC] Starting')
+        adoc = Toolchain::Adoc.load_doc(
+          CM.get('index.default.file')
+        ) if adoc.nil?
         parsed = adoc.parsed
         attributes = adoc.attributes
         lines = parsed.reader.source_lines
-        reader = ::Asciidoctor::PreprocessorReader.new parsed, lines
+        reader = ::Asciidoctor::PreprocessorReader.new(parsed, lines)
         combined_source = reader.read_lines
-        doc = ::Asciidoctor::Document.new combined_source, 
+        doc = ::Asciidoctor::Document.new(
+          combined_source,
           catalog_assets: true,
           sourcemap: true,
           safe: :unsafe,
           attributes: attributes
+        )
         doc.parse
 
         stack = [OpenStruct.new(id: 'root', level: -1, children: [])]
 
         doc.catalog[:refs].keys.each do |r|
           ref = doc.catalog[:refs][r]
+          puts ref
           level = ref.level
           title = ref.title
           id = ref.id
           attribs = ref.instance_variable_get(:@attributes)
-          isDiscrete = (attribs&.fetch(1) == 'discrete')
-          if isDiscrete
+          puts attribs
+          is_discrete = (attribs&.fetch(1) == 'discrete')
+          if is_discrete
             log('TOC', 'ID ' + id + ' is discrete', :grey)
             next
           end
@@ -64,30 +81,27 @@ module Toolchain
         toc_openstruct = stack.first
         toc_hash = openstruct_to_hash(toc_openstruct)
         toc_json = JSON.pretty_generate(toc_hash)
-        json_file = File.open(json_filepath, 'w+')
-        begin
+        File.open(json_filepath, 'w+') do |json_file|
           json_file.write(toc_json)
-        ensure
-          json_file.close
         end
-        log('TOC', 'File written to; ' + json_filepath, :gray)
+        log('TOC', 'File written to: ' + json_filepath, :gray)
         return json_filepath
       end
 
       private
 
       ## Takes OpenStruct +object+ and returns +hash+
-      # Useful for converting OpenStruct Hash for later conversion to JSON 
+      # Useful for converting OpenStruct Hash for later conversion to JSON
       #
       def openstruct_to_hash(object, hash = {})
-      object.each_pair do |key, value|
-        hash[key] = case value
+        object.each_pair do |key, value|
+          hash[key] = case value
                       when OpenStruct then openstruct_to_hash(value)
                       when Array then value.map { |v| openstruct_to_hash(v) }
                       else value
-                    end
-      end
-      hash
+                      end
+        end
+        return hash
       end
     end
   end
