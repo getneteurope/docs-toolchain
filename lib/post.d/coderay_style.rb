de7a2764b413b12f1c1a5818237ea4a32633ecde
Lukas Deutz <lukas.deutz@wirecard.com>
2020-03-18 13:54:02 +0100

    Add PostProcess CodeRayStyleCopy

---
+++ lib/post.d/coderay_style.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require_relative '../process_manager.rb'
+require_relative '../utils/paths.rb'
+require_relative '../log/log.rb'
+
+module Toolchain
+  ##
+  # Adds modules for postprocessing files.
+  module Post
+    ##
+    # Copies the CodeRay style defined in `content/css/asciidoctor-coderay.css`
+    # to the build dir.
+    class CodeRayStyleCopy < BaseProcess
+      def run
+        coderay_css = 'asciidoctor-coderay.css'
+        stage_log(:post, "Copying CodeRay stylesheet to HTML directory: #{coderay_css}")
+        ::FileUtils.cp(
+          ::File.join(::Toolchain.document_root, 'css', coderay_css),
+          ::File.join(::Toolchain.html_path, 'css', coderay_css)
+        )
+      end
+    end
+  end
+end
+
+Toolchain::PostProcessManager.instance.register(Toolchain::Post::CodeRayStyleCopy.new)
