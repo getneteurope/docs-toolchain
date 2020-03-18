8be08e8503933a3afedda0ee7345a185a1ae60e7
Lukas Deutz <lukas.deutz@wirecard.com>
2020-03-18 14:39:43 +0100

    Fix missing require

--- lib/post.d/coderay_style.rb
+++ lib/post.d/coderay_style.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require_relative '../base_process.rb'
 require_relative '../process_manager.rb'
 require_relative '../utils/paths.rb'
 require_relative '../log/log.rb'
