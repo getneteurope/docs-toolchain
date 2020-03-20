2f5c14a05849e9cbde66f91097e5e5c88f89098e
Lukas Deutz <lukas.deutz@wirecard.com>
2020-03-20 11:43:27 +0100

    Exclude slack.rb from unit test coverage * Not feasible to test external API

--- test/main.rb
+++ test/main.rb
@@ -4,6 +4,7 @@ require 'simplecov'
 SimpleCov.start do |config|
   add_filter 'test/'
   add_filter 'lib/stages/'
+  add_filter 'lib/notify/slack.rb' # not feasible to test
 end
 
 ENV['UNITTEST'] = 'true'
