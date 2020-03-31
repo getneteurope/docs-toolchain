d45aebbb0ba62c4dbd222f528113405d4f5755f5
Lukas Deutz <lukas.deutz@wirecard.com>
2020-03-31 11:43:23 +0200

    Add .simplecov

--- test/main.rb
+++ test/main.rb
@@ -1,11 +1,6 @@
 # frozen_string_literal: true
 
 require 'simplecov'
-SimpleCov.start do |config|
-  add_filter 'test/'
-  add_filter 'lib/stages/'
-  add_filter 'lib/notify/slack.rb' # not feasible to test
-end
 
 ENV['UNITTEST'] = 'true'
 Dir[File.join(__dir__, 'test_*.rb')].each { |f| require f }
