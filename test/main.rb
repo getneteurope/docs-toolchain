4cda5e0cfe8f6b36311c2245fb3444e68d971a8d
Lukas Deutz <lukas.deutz@wirecard.com>
2020-03-31 15:47:05 +0200

    Set correct filenames for lcov

--- test/main.rb
+++ test/main.rb
@@ -3,7 +3,10 @@
 require 'simplecov'
 require 'simplecov-lcov'
 
-SimpleCov::Formatter::LcovFormatter.config.report_with_single_file = true
+SimpleCov::Formatter::LcovFormatter.config do |config|
+  config.report_with_single_file = true
+  config.single_report_path = 'coverage/lcov/docs-toolchain.lcov'
+end
 
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::HTMLFormatter,
