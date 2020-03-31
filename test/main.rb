0e9694ff66fd16db0005068e7146f9388fb9b0b1
Lukas Deutz <lukas.deutz@wirecard.com>
2020-03-31 15:28:56 +0200

    Add SimpleCov-LCOV

--- test/main.rb
+++ test/main.rb
@@ -1,11 +1,21 @@
 # frozen_string_literal: true
 
-if ENV.key?('GITHUB_ACTIONS')
-  require 'coveralls'
-  Coveralls.wear!
-else
-  require 'simplecov'
+require 'simplecov'
+require 'simplecov-lcov'
+
+SimpleCov::Formatter::LcovFormatter.config.single_report_path = 'coverage/lcov.info'
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
+  SimpleCov::Formatter::HTMLFormatter,
+  SimpleCov::Formatter::LcovFormatter
+])
+
+SimpleCov.start do |config|
+  add_filter 'test/'
+  add_filter 'lib/stages/'
+  add_filter 'lib/notify/slack.rb' # not feasible to test
 end
+
 
 ENV['UNITTEST'] = 'true'
 Dir[File.join(__dir__, 'test_*.rb')].each { |f| require f }
