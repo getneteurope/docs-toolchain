getneteurope/docs-toolchain

commit 9070d0d6b62a24c7da1e16118a85b1dd16968ee9

Refactor test stage

lib/cli.rb
@@ -54,12 +54,14 @@ Defaults:
       ##
       # Parse arguments given as +argv+.
       #
-      # Returns hash containing all options as key=value pairs and the parser object.
+      # Returns hash containing all options as key=value pairs
+      # and the parser object.
       def self.parse_args(argv = ARGV)
-        args = { help: false, debug: false, index: nil, files: [] }
+        args = { help: false, debug: false, index: nil }
 
         opt_parser = OptionParser.new do |parser|
-          parser.banner = 'Usage: main.rb [options] [--index INDEX | --file FILE [--file FILE ...]]
+          parser.banner =
+            'Usage: main.rb [options] [--index INDEX | --file FILE [--file FILE ...]]
 Default: index file is \'content/index.adoc\''
 
           parser.on('-d', '--debug', 'enable debug mode') do
@@ -68,9 +70,6 @@ Default: index file is \'content/index.adoc\''
 
           parser.on('-iINDEX', '--index INDEX', 'specify an index file') do |index|
             args[:index] = index
-          end
-          parser.on('-fFILE', '--file FILE', 'specify multiple files instead of one index file') do |file|
-            args[:files] << file
           end
 
           parser.on('-h', '--help', 'print this help') do

bin/test.rb
@@ -24,14 +24,6 @@ def main(argv = ARGV)
   ### Print loaded modules
   print_loaded_extensions if args.debug
 
-  ### Run on file arguments
-  files = args.files
-  unless files.empty?
-    stage_log(:test, "Running file checks on file set: #{files}")
-    stage_log(:test, 'Will exit after this.')
-    test_files(files) # will exit if run
-  end
-
   ### Run checks on default files
   index_adoc = args.index || DEFAULT_INDEX
   log('ARGS', args)

lib/stages/test.rb
@@ -70,24 +70,6 @@ def print_errors(errors_map)
 end
 
 ##
-# Test all files given as +files+, +files+ must be a list of filenames.
-# Only used for testing individual files, i.e. '--file' parameter.
-#
-# Returns nothing, this function will exit.
-def test_files(files)
-  files.each do |f|
-    errors = run_tests(f)
-    next if errors.empty?
-
-    puts f.blue.bold
-    errors.each do |err|
-      puts "#{err[:id]}\t#{err[:msg]}".bold.red
-    end
-  end
-  exit 0
-end
-
-##
 # Run all extensions registered with +ExtensionManager+ on the file +filename+.
 #
 # During this process, the file +filename+ will be loaded, converted and cached

commit 5ce2a8f96efe70d12b661a3401dde33378a51a96

Fix errors introduced in 9070d0d6b62a24c7da1e16118a85b1dd16968ee9

lib/cli.rb
@@ -61,14 +61,15 @@ Defaults:
 
         opt_parser = OptionParser.new do |parser|
           parser.banner =
-            'Usage: main.rb [options] [--index INDEX | --file FILE [--file FILE ...]]
+            'Usage: main.rb [options] [--index INDEX]
 Default: index file is \'content/index.adoc\''
 
           parser.on('-d', '--debug', 'enable debug mode') do
             args[:debug] = true
           end
 
-          parser.on('-iINDEX', '--index INDEX', 'specify an index file') do |index|
+          parser.on('-iINDEX', '--index INDEX',
+            'specify an index file') do |index|
             args[:index] = index
           end
 
@@ -77,9 +78,6 @@ Default: index file is \'content/index.adoc\''
           end
         end
         opt_parser.parse!(argv)
-
-        err_msg = 'Cannot provide "file" and "index" arguments simultaneously. Pick one!'
-        raise ArgumentError, err_msg if args[:index] && !args[:files].empty?
 
         return OpenStruct.new(args), opt_parser
       end

test/test_cli_test.rb
@@ -10,7 +10,6 @@ class TestTestCLI < Test::Unit::TestCase
     args, = Toolchain::Test::CLI.parse_args([])
     assert_false(args.help)
     assert_false(args.debug)
-    assert_empty(args.files)
     assert_nil(args.index)
   end
 
@@ -18,7 +17,6 @@ class TestTestCLI < Test::Unit::TestCase
     args, = Toolchain::Test::CLI.parse_args(%w[--help])
     assert_true(args.help)
     assert_false(args.debug)
-    assert_empty(args.files)
     assert_nil(args.index)
   end
 
@@ -26,15 +24,6 @@ class TestTestCLI < Test::Unit::TestCase
     args, = Toolchain::Test::CLI.parse_args(%w[--debug])
     assert_false(args.help)
     assert_true(args.debug)
-    assert_empty(args.files)
-    assert_nil(args.index)
-  end
-
-  def test_files
-    args, = Toolchain::Test::CLI.parse_args(%w[--file test.adoc --file content.adoc])
-    assert_false(args.help)
-    assert_false(args.debug)
-    assert_equal(['test.adoc', 'content.adoc'], args.files)
     assert_nil(args.index)
   end
 
@@ -42,11 +31,6 @@ class TestTestCLI < Test::Unit::TestCase
     args, = Toolchain::Test::CLI.parse_args(%w[--index index.adoc])
     assert_false(args.help)
     assert_false(args.debug)
-    assert_empty(args.files)
     assert_equal('index.adoc', args.index)
-  end
-
-  def test_index_and_file
-    assert_raise(ArgumentError) { Toolchain::Test::CLI.parse_args(%w[--index index.adoc --file file.adoc]) }
   end
 end